fa65c8dcaa64ecd03a6355a76a8f3388fe5a7b95
MOBAI <mobai@duck.com>
2025-06-04 19:17:20 +0800

    feat: add image

--- sections/people/D3.tsx
+++ sections/people/D3.tsx
@@ -1,3 +1,21 @@
+import a from "@/assets/d3/a.jpeg";
+import b from "@/assets/d3/b.png";
+import c from "@/assets/d3/c.png";
+import d from "@/assets/d3/d.jpeg";
+import e from "@/assets/d3/e.jpeg";
+import f from "@/assets/d3/f.jpeg";
+
+const Block = ({ src }: { src: string }) => {
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="aspect-square overflow-hidden">
+        <img src={src} alt="Ryan" className="h-full w-full object-cover" />
+      </div>
+      {/* <p className="text-mobai-gray text-2xl font-bold">Ryan</p> */}
+    </div>
+  );
+};
+
 const D3 = () => {
   return (
     <div className="p-10">
@@ -5,6 +23,14 @@ const D3 = () => {
         <h2 className="text-mobai-gray decoration-mobai-gray font-serif text-3xl font-bold underline decoration-2 underline-offset-6 lg:text-5xl">
           People in Spark Lab
         </h2>
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
+          <Block src={a.src} />
+          <Block src={b.src} />
+          <Block src={c.src} />
+          <Block src={d.src} />
+          <Block src={e.src} />
+          <Block src={f.src} />
+        </div>
       </div>
     </div>
   );
