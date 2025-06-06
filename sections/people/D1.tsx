780d537e055fbb2e26599ac34e2c339959c56dd3
MOBAI <mobai@duck.com>
2025-06-06 17:39:22 +0800

    perf: png to webp

--- sections/people/D1.tsx
+++ sections/people/D1.tsx
@@ -1,4 +1,5 @@
-import a from "@/assets/d1/a.svg";
+import a from "@/assets/d1/a.webp";
+import b from "@/assets/d1/b.svg";
 
 const D1 = () => {
   return (
@@ -8,7 +9,12 @@ const D1 = () => {
         backgroundImage: `url(${a.src})`,
       }}
     >
-      <div className="relative mx-auto flex h-full w-full max-w-screen-xl flex-col items-center justify-center p-10">
+      <img
+        src={b.src}
+        alt=""
+        className="absolute inset-0 z-10 h-full w-full object-cover"
+      />
+      <div className="relative z-20 mx-auto flex h-full w-full max-w-screen-xl flex-col items-center justify-center p-10">
         <h1 className="font-title text-center text-4xl leading-tight font-bold tracking-tight whitespace-pre-line text-white drop-shadow-2xl sm:text-6xl lg:text-7xl xl:text-8xl">
           Find your spark by getting lost
         </h1>
